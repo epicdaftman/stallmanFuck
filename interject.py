d02a859331ea060ff5aa212bc0ace391aa64958d
daft <williamjrmoody@icloud.com>
2018-02-15 19:34:48 -0500

    Fixed issues with spacing

--- interject.py
+++ interject.py
@@ -5,33 +5,33 @@ d = {}
 opening = 'I\'d just like to interject for moment.'
 
 d['<'] = ('What you\'re refering to as Linux, is in fact, GNU/Linux, or as'
-          'I\'ve recently taken to calling it, GNU plus Linux.')
+          ' I\'ve recently taken to calling it, GNU plus Linux.')
 
 d['>'] = ('Linux is not an operating system unto itself, but rather another'
           ' free component of a fully functioning GNU system made useful by'
-          'the GNU corelibs, shell utilities and vital system components co'
+          ' the GNU corelibs, shell utilities and vital system components co'
           'mprising a full OS as defined by POSIX.')
 
 d['+'] = ('Many computer users run a modified version of the GNU system'
-          'every day, without realizing it.')
+          ' every day, without realizing it.')
 
 d['-'] = ('Through a peculiar turn of events, the version of GNU which is'
-          'widely used today is often called Linux, and many of its users are'
-          'not aware that it is basically the GNU system, developed by the'
-          'GNU Project.')
+          ' widely used today is often called Linux, and many of its users are'
+          ' not aware that it is basically the GNU system, developed by the'
+          ' GNU Project.')
 
 d['.'] = ('There really is a Linux, and these people are using it, but it is'
-          'just a part of the system they use.')
+          ' just a part of the system they use.')
 
 d[','] = ('Linux is the kernel: the program in the system that allocates the'
-          'machine\'s resources to the other programs that you run.')
+          ' machine\'s resources to the other programs that you run.')
 
 d['['] = ('The kernel is an essential part of an operating system, but useless'
-          'by itself; it can only function in the context of a complete'
-          'operating system.')
+          ' by itself; it can only function in the context of a complete'
+          ' operating system.')
 
 d[']'] = ('Linux is normally used in combination with the GNU operating system'
-          ': the whole system is basically'
+          ' : the whole system is basically'
           ' GNU with Linux added, or GNU/Linux.')
 
 closing = ('All the so-called Linux distributions are really distributions of'
